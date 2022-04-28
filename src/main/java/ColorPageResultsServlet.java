2a3e8ef7ddeece8aa6e267b88f0daddcf2156810
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-27 20:56:19 -0500

    feat: created new servlet resp. for display color-viewer.jsp

---
+++ src/main/java/ColorPageResultsServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ColorPageResultsServlets", urlPatterns = "/viewcolor")
+public class ColorPageResultsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("/color-viewer.jsp").forward(request, response);
+
+    }
+
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+
+}
