db0c7aafe79e48de0f47344fe46f480c47822e6b
Oscar Castro <oscar.a.castro818@gmail.com>
2022-05-01 23:24:04 -0500

    feat: web servlet for logic resp. for username and password credentials

---
+++ src/main/java/AdServlet.java
@@ -0,0 +1,27 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "AdServlet", urlPatterns = "/ads")
+public class AdServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+//        Ads adsDao = DaoFactory.getAdsDao();
+//        List<Ad> ads = adsDao.all();
+        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
+
+    }
+
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//
+//    }
+}
+
