2cdfca30b3748a6306f9a365d7b06ca3afdb45a5
Oscar Castro <oscar.a.castro818@gmail.com>
2022-05-02 14:03:53 -0500

    feat: created 'user' key for the session, redirected unauthorized users to login

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -8,18 +8,38 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+
+///////////////////  example  /////////////////////////////////
+//        if ((boolean)request.getSession().getAttribute("isAdmin") == false) {
+//            response.sendRedirect("/login");
+//            return;
+//        }
+//        request.getRequestDispatcher("/secret-admin-page.jsp").forward(request, response);
+//    }
+        if (request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
+///////////////////  example  /////////////////////////////////
+//        if (isAdmin) {
+//            request.getSession().setAttribute("isAdmin", true);
+//        }
         if (validAttempt) {
+            request.getSession().setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
+
+
     }
 }
